1871f5d7667d17b2abbaf7f08cd6d2fb2b9a3113
HardProg <42470189+HardProg@users.noreply.github.com>
2018-08-17 19:19:51 +0600

    Add files via upload

---
+++ HSV_example.ino
@@ -0,0 +1,20 @@
+//This is an example of HSV write
+
+#include <Adafruit_NeoPixel.h>
+#define PIN 7
+#define LED_NUM 100
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_NUM, PIN, NEO_GRB + NEO_KHZ800); //Левая лента
+
+void setup() {
+  strip.begin();
+  strip.show();
+  for (int i; i < 50; i++) {
+    strip.setPixelColorHsv(i, i*30, 255, i*5);
+  }
+  strip.show();
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
